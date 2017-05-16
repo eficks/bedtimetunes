a8825d1a26c6c371884ada7416faee351fe9478c
Lewi Hirvela <lewi.hirvela@gmail.com>
2017-05-16 20:59:39 +0300

    Playlist Updates

--- src/app/components/tunes/tunes.component.ts
+++ src/app/components/tunes/tunes.component.ts
@@ -17,19 +17,30 @@ export interface IMedia {
 export class TunesComponent {
   sources: Array<IMedia> = SOURCES;
   currentIndex = 0;
+  previousIndex = 0;
+  nowPlaying = 0 ;
+  state = 'playing';
+  previousItem: IMedia = this.sources[ this.currentIndex ];
   currentItem: IMedia = this.sources[ this.currentIndex ];
   api: VgAPI;
   constructor() {
+
   }
 
   onPlayerReady(api: VgAPI) {
       this.api = api;
+      this.currentItem = this.sources[this.currentIndex];
+      let pauseState = <HTMLElement> document.querySelector("#playlist_item_" + this.currentItem.id);
+      pauseState.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
 
       this.api.getDefaultMedia().subscriptions.loadedMetadata.subscribe(this.playVideo.bind(this));
       this.api.getDefaultMedia().subscriptions.ended.subscribe(this.nextVideo.bind(this));
+
   }
 
   nextVideo() {
+      this.previousIndex = this.currentIndex;
+
       this.currentIndex++;
 
       if (this.currentIndex === this.sources.length) {
@@ -37,16 +48,49 @@ export class TunesComponent {
       }
 
       this.currentItem = this.sources[this.currentIndex];
+      this.previousItem = this.sources[this.previousIndex];
+
+      let pauseState = <HTMLElement> document.querySelector("#playlist_item_" + this.currentItem.id);
+      pauseState.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+      let playState = <HTMLElement> document.querySelector("#playlist_item_" + this.previousItem.id);
+      playState.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
   }
 
   playVideo() {
-      this.api.play();
+      this.nowPlaying = this.currentIndex;
+      this.api.play()
+
   }
 
-  onClickPlaylistItem(item: IMedia, index: number) {
+  onClickPlaylistItem(item: IMedia, index: number, previousIndex: number) {
       this.currentIndex = index;
       this.currentItem = item;
+      this.previousIndex = previousIndex;
+      this.previousItem = this.sources[this.previousIndex];
       this.api.play();
+      if (this.currentItem != this.previousItem) {
+        let pauseState = <HTMLElement> document.querySelector("#playlist_item_" + this.currentItem.id);
+        pauseState.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+        this.currentItem = this.sources[this.currentIndex];
+        let playState = <HTMLElement> document.querySelector("#playlist_item_" + this.previousItem.id);
+        playState.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+      } else {
+        if (this.state=='playing') {
+              let playState = <HTMLElement> document.querySelector("#playlist_item_" + this.previousItem.id);
+              playState.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+              this.api.pause();
+              this.state = 'paused';
 
-  }
+        } else {
+              let pauseState = <HTMLElement> document.querySelector("#playlist_item_" + this.currentItem.id);
+              pauseState.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+              this.api.play();
+              this.state = 'playing';
+            
+        }
+      }
+      console.log(this.state);
+      console.log(this.previousItem.title);
+
+      }
 }
--- src/app/components/tunes/tunes.ts
+++ src/app/components/tunes/tunes.ts
@@ -11,7 +11,7 @@ export const SOURCES = [
     id: 1,
     title: "Creep",
     artist: "Radiohead",
-    src: "/assets/music/test.mp3",
+    src: "/assets/music/test1.mp3",
     type: "audio/mp3"
 },
 {
@@ -25,28 +25,28 @@ export const SOURCES = [
     id: 3,
     title: "To Shiela",
     artist: "Smashing Pumpkins",
-    src: "/assets/music/test.mp3",
+    src: "/assets/music/test2.mp3",
     type: "audio/mp3"
 },
 {
     id: 4,
     title: "Creep",
     artist: "Radiohead",
-    src: "/assets/music/test.mp3",
+    src: "/assets/music/test3.mp3",
     type: "audio/mp3"
 },
 {
     id: 5,
     title: "Perfect Drug",
     artist: "Nine Inch Nails",
-    src: "http://static.videogular.com/assets/audios/videogular.mp3",
+    src: "/assets/music/test4.mp3",
     type: "audio/mp3"
 },
 {
     id: 6,
     title: "To Shiela",
     artist: "Smashing Pumpkins",
-    src: "/assets/music/test.mp3",
+    src: "/assets/music/test5.mp3",
     type: "audio/mp3"
 }
 ];
